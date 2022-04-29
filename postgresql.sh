9200fc7c87286a46e9274242d26f8a4fe0207d4f
Gabriel <102559742+gabchav@users.noreply.github.com>
2022-04-28 20:07:03 -0400

    Update postgresql.sh

--- postgresql.sh
+++ postgresql.sh
@@ -7,6 +7,30 @@ sudo su -postgres psql
 #Agregar contraseña al usuario postgres
 ALTER USER postgres WITH PASSWORD '123';
 
+#Crear un usuario:
+CREATE USER username WITH password '123';
+
+#Crear una base de datos y asignarla al usuario creado anteriormente:
+CREATE DATABASE basededatos WITH owner = username;
+
+#Conectarse a la base datos con el usuario creado
+psql -U username -h 127.0.0.1 -d basededatos
+
+#Crear una base de datos:
+CREATE DATABASE basededatos;
+
+#Asignar el usuario a la base de datos
+GRANT ALL PRIVILEGES ON DATABASE basededatos TO username;
+
+#Asignar super privelegios
+ALTER USER username WITH SUPERUSER;
+
+#Eliminar base datos
+DROP DATABASE basededatos;
+
+#Importar una base datos
+psql username -h servidor -d basededatos < respaldo.sql
+
 #Listar base de datos
 \l
 
@@ -19,5 +43,17 @@ ALTER USER postgres WITH PASSWORD '123';
 #Seleccionar o cambiar base de datos
 \c otrabasededatos
 
+#Listar usuarios
+\du
+
+#Eliminar usuario
+DROP USER nomusr
+
+#Obtener ayuda
+\?
+
 #Salir de postgres
 \q
+
+#Cambiar Puerto o Conectarse a un servidor diferente
+# se debe cambiar en el archivo postgresql.conf y pg_hba.conf
